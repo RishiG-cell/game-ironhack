b2b8cc8254d73a3e1ef3a80a123a9d5ef9171e2f
Rishi G. <mail.rishig@gmail.com>
2025-05-18 22:06:24 +0200

    updates game screen 1 and 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const gameIntro = document.querySelector("#game-intro");
 const startBttn = document.querySelector("#start");
 const gameScreen = document.querySelector("#game-container");
+const audio = document.querySelector("#audio");
 
 startBttn.addEventListener("click", () => {
   startGame();
@@ -9,4 +10,6 @@ startBttn.addEventListener("click", () => {
 function startGame() {
   gameIntro.style.display = "none";
   gameScreen.style.display = "flex";
+  audio.play();
+  audio.volume = 0.01;
 }
