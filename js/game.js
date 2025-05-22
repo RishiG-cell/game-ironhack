4fec6012c39b1c7aca557ef94f48f67651df6174
Rishi G. <mail.rishig@gmail.com>
2025-05-22 16:07:49 +0200

    final result added egg

--- js/game.js
+++ js/game.js
@@ -14,6 +14,7 @@ class Game {
     this.obstacles = [new Obstacle(this.gameScreen, 30, 40)];
     this.orange = [new Orange(this.gameScreen, 50, 40)];
     this.venomObj = [];
+    this.bottlePoints = [];
     this.score = 0;
     this.lives = 3;
     this.gameIsOver = false;
@@ -110,6 +111,9 @@ class Game {
         if (this.score % 10 === 0 && this.score !== 0) {
           this.venomObj.push(new Venom(this.gameScreen, 40, 70));
         }
+        if (this.score % 15 === 0 && this.score !== 0) {
+          this.bottlePoints.push(new BottleEgg(this.gameScreen, 100, 70));
+        }
         this.scoreElm.innerText = this.score;
         if (this.score % 2 === 0) {
           this.amountObs = this.amountObs - 5;
@@ -122,6 +126,28 @@ class Game {
       if (currentObstacle.top > 800) {
         currentObstacle.element.remove();
         this.orange.splice(i, 1);
+        i--;
+      }
+    }
+
+    for (let i = 0; i < this.bottlePoints.length; i++) {
+      const currentObstacle = this.bottlePoints[i];
+      currentObstacle.move();
+
+      if (this.player.didCollide(currentObstacle)) {
+        currentObstacle.element.remove();
+        this.bottlePoints.splice(i, 1);
+        i++;
+        this.score = this.score + 10;
+        this.scoreElm.innerText = this.score;
+        if (this.lives === 0) {
+          this.gameIsOver = true;
+        }
+      }
+
+      if (currentObstacle.top > 800) {
+        currentObstacle.element.remove();
+        this.bottlePoints.splice(i, 1);
         i--;
       }
     }
@@ -147,7 +173,7 @@ class Game {
         currentObstacle.element.remove();
         this.venomObj.splice(i, 1);
         i--;
-        this.lives--;
+        this.lives = 0;
         this.livesElement.innerText = this.lives;
         this.player.element.classList.add("spin");
         setTimeout(() => {
@@ -171,6 +197,9 @@ class Game {
       oneObstacle.element.remove();
     });
     this.orange.forEach((oneObstacle) => {
+      oneObstacle.element.remove();
+    });
+    this.bottlePoints.forEach((oneObstacle) => {
       oneObstacle.element.remove();
     });
     this.player.beer.forEach((oneObstacle) => {
